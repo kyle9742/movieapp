4ca4c4a05c4e6017cd8b01dc26834b9a91625d40
it <kyle9742@naver.com>
2024-06-24 15:54:59 +0900

    TheMovieDB api 로 영화 불러오기

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieList from './components/MovieList/MovieList';
+import Navbar from './components/Navbar/Navbar';
+
+export default function App() {
+	return (
+		<div className='app'>
+			<Navbar />
+
+			<MovieList />
+		</div>
+	);
+}
