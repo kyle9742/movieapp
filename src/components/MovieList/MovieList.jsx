d09921cb9a38c9b8afa948510fdb22094c67e7c4
it <kyle9742@naver.com>
2024-06-24 16:28:24 +0900

    lodash 이용해서 정렬

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,9 +2,28 @@ import "./MovieList.css";
 import Fire from "../../assets/fire.png";
 import MovieCard from "./MovieCard";
 import { useEffect, useState } from "react";
+import _ from "lodash";
 
 export default function MovieList() {
   const [movies, setMovies] = useState([]);
+  const [filterMovies, setFilterMovies] = useState([]);
+  const [minRating, setMinRating] = useState(0);
+  const [sort, setSort] = useState({
+    by: "default",
+    order: "asc",
+  });
+
+  const handleSort = (e) => {
+    const { name, value } = e.target;
+    setSort((prev) => ({ ...prev, [name]: value }));
+  };
+
+  useEffect(() => {
+    if (sort.by !== 'default') {
+        const sortedMovies = _.orderBy(filterMovies, [sort.by], [sort.order]);
+        setFilterMovies(sortedMovies);
+    }
+  }, [sort]);
 
   useEffect(() => {
     fetchMovies();
@@ -14,6 +33,18 @@ export default function MovieList() {
     const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=1cd36bf9e39e2e65a9f986373c5a6321&language=ko");
     const data = await response.json();
     setMovies(data.results);
+    setFilterMovies(data.results);
+  };
+
+  const handleFilter = (rate) => {
+    if (minRating === rate) {
+      setMinRating(0);
+      setFilterMovies(movies);
+    } else {
+      setMinRating(rate);
+      const filtered = movies.filter((movie) => movie.vote_average >= rate);
+      setFilterMovies(filtered);
+    }
   };
 
   return (
@@ -25,25 +56,31 @@ export default function MovieList() {
 
         <div className="align_center movie_list_fs">
           <ul className="align_center movie_filter">
-            <li className="movie_filter_item active">8+ Star</li>
-            <li className="movie_filter_item">7+ Star</li>
-            <li className="movie_filter_item">6+ Star</li>
+            <li className={minRating === 8 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(8)}>
+              8+ Star
+            </li>
+            <li className={minRating === 7 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(7)}>
+              7+ Star
+            </li>
+            <li className={minRating === 6 ? "movie_filter_item active" : "movie_filter_item"} onClick={() => handleFilter(6)}>
+              6+ Star
+            </li>
           </ul>
 
-          <select name="" id="" className="movie_sorting">
-            <option value="">SortBy</option>
-            <option value="">Date</option>
-            <option value="">Rating</option>
+          <select name="by" id="" onChange={handleSort} className="movie_sorting">
+            <option value="default">SortBy</option>
+            <option value="release_date">Date</option>
+            <option value="vote_average">Rating</option>
           </select>
-          <select name="" id="" className="movie_sorting">
-            <option value="">Ascending</option>
-            <option value="">Descending</option>
+          <select name="order" id="" onChange={handleSort} className="movie_sorting">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
           </select>
         </div>
       </header>
 
       <div className="movie_cards">
-        {movies.map((movie) => (
+        {filterMovies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
